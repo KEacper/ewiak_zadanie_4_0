58bc1fe51212638ef61910dd3e7a91e878d7cf6b
KEacper <101186813+KEacper@users.noreply.github.com>
2022-05-31 11:06:00 +0000

    4_0 prawie działa

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.io.IOException;
 
 class WrongStudentName extends Exception { }
+class WrongAge extends Exception { }
 
 class Main {
     public static Scanner scan = new Scanner(System.in);
@@ -21,6 +22,8 @@ class Main {
 
             } catch(WrongStudentName e) {
                 System.out.println("Błędne imie studenta!");
+            } catch(WrongAge e) {
+                System.out.println("Błędny wiek studenta!");
             }
         }
     }
@@ -43,16 +46,32 @@ class Main {
 
         return name;
     }
-
-    public static void exercise1() throws IOException, WrongStudentName {
-        var name = ReadName();
+      public static int ReadAge() throws WrongAge {
         System.out.println("Podaj wiek: ");
         var age = scan.nextInt();
-        scan.nextLine();
+        if(age < 100 && age > 100)
+            throw new WrongAge();
+
+        return age;
+    }
+
+    public static void exercise1() throws IOException, WrongStudentName, WrongAge {
+        var name = ReadName();
+        var age = ReadAge();
         System.out.println("Podaj datę urodzenia DD-MM-YYY");
         var date = scan.nextLine();
         (new Service1()).addStudent(new Student(name, age, date));
     }
+
+  // public static void exercise1() throws IOException, WrongStudentName {
+  //       var name = ReadName();
+  //       System.out.println("Podaj wiek: ");
+  //       var age = scan.nextInt();
+  //       scan.nextLine();
+  //       System.out.println("Podaj datę urodzenia DD-MM-YYY");
+  //       var date = scan.nextLine();
+  //       (new Service1()).addStudent(new Student(name, age, date));
+  //   }
 
     public static void exercise2() throws IOException {
         var students = (new Service1()).getStudents();
@@ -60,6 +79,7 @@ class Main {
             System.out.println(current.ToString());
         }
     }
+  
 
     public static void exercise3() throws IOException {
         scan.nextLine();
